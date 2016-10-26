e56a61ac4b331c325f76cb688d12ce79c41f8a46
Frank Schüler <SchuelerFrank@gmx.de>
2016-10-26 10:10:36 +0200

    prepare release 2.5.0

--- de.titus.jquery.plugins-2.5.0.js
+++ de.titus.jquery.plugins-2.5.0.js
@@ -1479,6 +1479,106 @@ de.titus.core.Namespace.create("de.titus.core.UUID", function() {
 		$.fn.de_titus_core_StringUtils = de.titus.core.StringUtils;
 	});
 })($);
+(function($) {
+	de.titus.core.Namespace.create("de.titus.core.EventBind", function() {
+		"use strict";
+		de.titus.core.EventBind = function(anElement, aContext) {
+			if (anElement.data(de.titus.core.EventBind.STATE.FINISHED) == undefined) {
+				
+				var eventType = anElement.attr("event-type");
+				if (eventType == undefined || eventType.trim().length == 0) {
+					anElement.data(de.titus.core.EventBind.STATE.FINISHED, de.titus.core.EventBind.FINISHEDSTATE.FAIL);
+					return this;
+				}
+				
+				var action = anElement.attr("event-action");
+				if (action == undefined || action.trim().length == 0) {
+					anElement.data(de.titus.core.EventBind.STATE.FINISHED, de.titus.core.EventBind.FINISHEDSTATE.FAIL);
+					return this;
+				}
+				
+				var data = undefined;
+				var eventData = anElement.attr("event-data");
+				if (eventData != undefined && eventData.trim().length > 0){
+					data = de.titus.core.EventBind.EXPRESSIONRESOLVER.resolveExpression(eventData, aContext , {});
+				}
+				else if(aContext != undefined){
+					data = $().extend({}, aContext);
+				}
+				else {
+					data = {};
+				}
+				
+				anElement.on(eventType, null, data, de.titus.core.EventBind.$$__execute__$$);
+				anElement.data(de.titus.core.EventBind.STATE.FINISHED, de.titus.core.EventBind.FINISHEDSTATE.READY);
+				return this;
+			}
+		};
+		
+		de.titus.core.EventBind.EXPRESSIONRESOLVER = new de.titus.core.ExpressionResolver();
+		de.titus.core.EventBind.STATE = {
+			FINISHED : "$$EventBind.FINISHED$$"
+		};
+		de.titus.core.EventBind.FINISHEDSTATE = {
+		FAIL : "fail",
+		READY : "ready"
+		};
+		
+		de.titus.core.EventBind.$$__execute__$$ = function(anEvent) {
+			var element = $(this);
+			if (element.attr("event-prevent-default") != undefined)
+				anEvent.preventDefault();
+			if (element.attr("event-stop-propagation") != undefined)
+				anEvent.stopPropagation();
+			
+			var action = element.attr("event-action");
+			action = de.titus.core.EventBind.EXPRESSIONRESOLVER.resolveExpression(action, anEvent.data, undefined);
+			if (typeof action === "function")
+				action(anEvent, anEvent.data);
+			
+			return !anEvent.isDefaultPrevented();
+		};
+		
+		$.fn.de_titus_core_EventBind = function(aContext) {
+			if (this.length == 1)
+				return de.titus.core.EventBind(this, aContext);
+			else if (this.length >= 1) {
+				return this.each(function() {
+					return $(this).de_titus_core_EventBind(aContext);
+				});
+			}
+		};
+		
+		$(document).ready(function() {
+			var hasAutorun = $("[event-autorun]");
+			if (hasAutorun != undefined && hasAutorun.length != 0) {
+				$("[event-autorun]").de_titus_core_EventBind();
+				$("[event-autorun]").find("[event-type]").de_titus_core_EventBind();
+				
+				var observer = new MutationObserver(function(mutations) {
+					mutations.forEach(function(mutation) {
+						for (var i = 0; i < mutation.addedNodes.length; i++) {
+							if (mutation.addedNodes[i].nodetype != Node.TEXT_NODE) {
+								$(mutation.addedNodes[i]).find("[event-type]").de_titus_core_EventBind();
+							}
+						}
+					});
+				});
+				
+				// configuration of the observer:
+				var config = {
+				attributes : false,
+				childList : true,
+				subtree : true,
+				characterData : false
+				};
+				
+				// pass in the target node, as well as the observer options
+				observer.observe(document.querySelector("[event-autorun]"), config);
+			}
+		});
+	});
+})($, document);
 
 /*
  * The MIT License (MIT)
@@ -2613,6 +2713,72 @@ de.titus.core.Namespace.create("de.titus.jstl.functions.AddAttribute", function(
 	};
 	
 });
+de.titus.core.Namespace.create("de.titus.jstl.functions.Databind", function() {
+	de.titus.jstl.functions.Databind = function() {
+	};
+	de.titus.jstl.functions.Databind.prototype = new de.titus.jstl.IFunction("databind");
+	de.titus.jstl.functions.Databind.prototype.constructor = de.titus.jstl.functions.Databind;
+	
+	/**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
+	 * static variables
+	 *********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+	de.titus.jstl.functions.Databind.LOGGER = de.titus.logging.LoggerFactory.getInstance().newLogger("de.titus.jstl.functions.Databind");
+	
+	/**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
+	 * functions
+	 *********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+	
+	de.titus.jstl.functions.Databind.prototype.run = function(aElement, aDataContext, aProcessor) {
+		if (de.titus.jstl.functions.Databind.LOGGER.isDebugEnabled())
+			de.titus.jstl.functions.Databind.LOGGER.logDebug("execute run(" + aElement + ", " + aDataContext + ", " + aProcessor + ")");
+		
+		var expressionResolver = aProcessor.expressionResolver || new de.titus.core.ExpressionResolver();
+		
+		var varname = this.getVarname(aElement, aDataContext, aProcessor, expressionResolver);
+		if (varname != undefined && varname.trim().length != 0) {
+			var value = this.getValue(aElement, aDataContext, aProcessor, expressionResolver);
+			if(value != undefined)
+				aElement.data(varname, value);			
+		}
+		
+		return new de.titus.jstl.FunctionResult(true, true);
+	};
+	
+	de.titus.jstl.functions.Databind.prototype.getVarname = function(aElement, aDataContext, aProcessor, anExpressionResolver) {
+		return aElement.attr(aProcessor.config.attributePrefix + this.attributeName + "-name");
+	};
+	
+	de.titus.jstl.functions.Databind.prototype.getValue = function(aElement, aDataContext, aProcessor, anExpressionResolver) {
+		var valueString =  aElement.attr(aProcessor.config.attributePrefix + this.attributeName);
+		
+		return anExpressionResolver.resolveExpression(valueString, aDataContext, undefined);
+	};
+});
+de.titus.core.Namespace.create("de.titus.jstl.functions.Eventbind", function() {
+	de.titus.jstl.functions.Eventbind = function() {
+	};
+	de.titus.jstl.functions.Eventbind.prototype = new de.titus.jstl.IFunction("eventbind");
+	de.titus.jstl.functions.Eventbind.prototype.constructor = de.titus.jstl.functions.Eventbind;
+	
+	/**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
+	 * static variables
+	 *********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+	de.titus.jstl.functions.Eventbind.LOGGER = de.titus.logging.LoggerFactory.getInstance().newLogger("de.titus.jstl.functions.Eventbind");
+	
+	/**********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************
+	 * functions
+	 *********************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************************/
+	
+	de.titus.jstl.functions.Eventbind.prototype.run = function(aElement, aDataContext, aProcessor) {
+		if (de.titus.jstl.functions.Eventbind.LOGGER.isDebugEnabled())
+			de.titus.jstl.functions.Eventbind.LOGGER.logDebug("execute run(" + aElement + ", " + aDataContext + ", " + aProcessor + ")");
+		
+		aElement.de_titus_core_EventBind(aDataContext);
+		
+		return new de.titus.jstl.FunctionResult(true, true);
+	};
+	
+});
 (function($) {
 	de.titus.core.Namespace.create("de.titus.jstl.Processor", function() {
 		
@@ -2864,11 +3030,12 @@ de.titus.core.Namespace.create("de.titus.jstl.Setup", function() {
 	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.Data());
 	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.Include());
 	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.Choose());
-	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.Foreach());
+	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.Foreach());	
 	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.AddAttribute());
+	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.Databind());
+	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.Eventbind());
 	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.TextContent());
 	de.titus.jstl.FunctionRegistry.getInstance().add(new de.titus.jstl.functions.AttributeContent());
-	
 });
 
 de.titus.core.Namespace.create("de.titus.jquery.jstl.plugin", function() {
